06ae93173015ae3d9d50d2f22f866fb053b68d67
PMLLLLL <pengmaolin@proton.me>
2025-03-11 15:46:39 +0800

    小改动

--- PythonMachineLearningCode/char09/CART.py
+++ PythonMachineLearningCode/char09/CART.py
@@ -1,4 +1,5 @@
 from numpy import *
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName):      #general function to parse tab -delimited floats
     dataMat = []                #assume last column is target value
@@ -18,7 +19,6 @@ def binSplitDataSet(dataSet, feature, value):
 
 
 def regLeaf(dataSet):#returns the value used for each leaf
-    print(dataSet[:,-1])
     return mean(dataSet[:,-1])
 
 def regErr(dataSet):
@@ -27,7 +27,6 @@ def regErr(dataSet):
 def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
     tolS = ops[0]; tolN = ops[1]
     #if all the target variables are the same value: quit and return value
-    print(dataSet[:,-1].T.tolist())
     if len(set(dataSet[:,-1].T.tolist()[0])) == 1: #exit cond 1
         return None, leafType(dataSet)
     m,n = shape(dataSet)
@@ -35,7 +34,7 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
     S = errType(dataSet)
     bestS = inf; bestIndex = 0; bestValue = 0
     for featIndex in range(n-1):
-        for splitVal in set(dataSet[:,featIndex]):
+        for splitVal in set(asarray(dataSet)[:,featIndex]):
             mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
             if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): continue
             newS = errType(mat0) + errType(mat1)
@@ -64,8 +63,42 @@ def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):#assume dat
     retTree['right'] = createTree(rSet, leafType, errType, ops)
     return retTree
 
-
+# 对于ex00数据集
 myData = loadDataSet('ex00.txt')
 
 myMat = asmatrix(myData)
-print(createTree(myMat))
+
+fig = plt.figure()
+
+
+# 绘图
+ax = fig.add_subplot(1,1,1)
+ax.scatter(myMat[:,0].T.A, myMat[:,1].T.A,s=2,c='red')
+ax.set_title('ex00.txt')
+
+tt = createTree(myMat)
+
+print(tt)
+
+
+plt.show()
+
+# # 对于ex0数据集
+# myData = loadDataSet('ex0.txt')
+#
+# myMat = asmatrix(myData)
+#
+# fig = plt.figure()
+#
+#
+# # 绘图
+# ax = fig.add_subplot(1,1,1)
+# ax.scatter(myMat[:,1].T.A, myMat[:,2].T.A,s=2,c='red')
+# ax.set_title('ex0.txt')
+#
+# tt = createTree(myMat)
+#
+# print(tt)
+#
+#
+# plt.show()
